90be094d2fc44966848e468272866fb57807a8af
Heng Li <lh3@me.com>
2013-10-18 14:01:37 -0400

    retrieve both strands with sub

--- sub.c
+++ sub.c
@@ -9,20 +9,32 @@ static inline void set_bit(uint64_t *bits, uint64_t k)
 	__sync_or_and_fetch(p, k);
 }
 
-static void set_bits(const rld_t *e, const uint64_t *sub, uint64_t *bits, int start, int step)
+static inline int64_t set_bits_aux(const rld_t *e, uint64_t *bits, uint64_t k)
 {
-	uint64_t i, k, *ok;
 	int c;
+	uint64_t *ok;
 	ok = alloca(8 * e->asize);
+	do {
+		set_bit(bits, k);
+		c = rld_rank1a(e, k + 1, ok);
+		k = e->cnt[c] + ok[c] - 1;
+		if (c == 0) break;
+	} while (c);
+	return k;
+}
+
+static int64_t set_bits(const rld_t *e, const uint64_t *sub, uint64_t *bits, int start, int step, int is_both)
+{
+	uint64_t i, k, n_ss = 0;
 	for (i = start; i < e->mcnt[1]; i += step) {
 		if ((sub[i>>6]>>(i&0x3f)&1) == 0) continue;
-		for (k = i;;) {
-			set_bit(bits, k);
-			c = rld_rank1a(e, k + 1, ok);
-			if (c == 0) break;
-			else k = e->cnt[c] + ok[c] - 1;
+		k = set_bits_aux(e, bits, i);
+		if (is_both && (sub[k>>6]>>(k&0x3f)&1) == 0) {
+			set_bits_aux(e, bits, k);
+			++n_ss;
 		}
 	}
+	return n_ss;
 }
 
 static rld_t *gen_idx(rld_t *e0, uint64_t *bits, int is_comp)
@@ -56,7 +68,8 @@ typedef struct {
 	const rld_t *e;
 	const uint64_t *sub;
 	uint64_t *bits;
-	int n_threads;
+	int64_t n_ss;
+	int n_threads, is_both;
 } shared_t;
 
 void kt_for(int n_threads, void (*func)(void*,int,int), void *data, int n);
@@ -64,18 +77,21 @@ void kt_for(int n_threads, void (*func)(void*,int,int), void *data, int n);
 static void worker(void *data, int i, int tid)
 {
 	shared_t *d = (shared_t*)data;
-	set_bits(d->e, d->sub, d->bits, i, d->n_threads);
+	int64_t x;
+	x = set_bits(d->e, d->sub, d->bits, i, d->n_threads, d->is_both);
+	__sync_fetch_and_add(&d->n_ss, x);
 }
 
-rld_t *fm_sub(rld_t *e, const uint64_t *sub, int n_threads, int is_comp)
+rld_t *fm_sub(rld_t *e, const uint64_t *sub, int n_threads, int is_comp, int is_both)
 {
 	shared_t d;
 	rld_t *r;
 	d.bits = calloc((e->mcnt[0] + 63) / 64, 8);
-	d.sub = sub, d.e = e, d.n_threads = n_threads;
+	d.sub = sub, d.e = e, d.n_threads = n_threads, d.is_both = is_both, d.n_ss = 0;
 	kt_for(n_threads, worker, &d, n_threads);
 	r = gen_idx(e, d.bits, is_comp);
 	free(d.bits);
+	if (is_both) fprintf(stderr, "[M::%s] # single-stranded: %ld\n", __func__, (long)d.n_ss);
 	return r;
 }
 
@@ -83,17 +99,18 @@ rld_t *fm_sub(rld_t *e, const uint64_t *sub, int n_threads, int is_comp)
 
 int main_sub(int argc, char *argv[])
 {
-	int c, is_comp = 0, n_threads = 1;
+	int c, is_comp = 0, is_both = 1, n_threads = 1;
 	rld_t *e;
 	uint64_t n_seqs, *sub;
 	FILE *fp;
 
-	while ((c = getopt(argc, argv, "ct:")) >= 0) {
+	while ((c = getopt(argc, argv, "sct:")) >= 0) {
 		if (c == 'c') is_comp = 1;
+		else if (c == 's') is_both = 0;
 		else if (c == 't') n_threads = atoi(optarg);
 	}
 	if (optind + 2 > argc) {
-		fprintf(stderr, "Usage: fermi2 sub [-c] [-t nThreads=1] <reads.rld> <bits.bin>\n");
+		fprintf(stderr, "Usage: fermi2 sub [-cs] [-t nThreads=1] <reads.rld> <bits.bin>\n");
 		return 1;
 	}
 	e = rld_restore(argv[optind]);
@@ -107,7 +124,7 @@ int main_sub(int argc, char *argv[])
 	sub = malloc((n_seqs + 63) / 64 * 8);
 	fread(sub, 8, (n_seqs + 63) / 64, fp);
 	fclose(fp);
-	e = fm_sub(e, sub, n_threads, is_comp);
+	e = fm_sub(e, sub, n_threads, is_comp, is_both);
 	free(sub);
 	rld_dump(e, "-");
 	rld_destroy(e);
