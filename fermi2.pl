86484ac810986534314df518394ffe39aafd5dc7
Heng Li <lh3@me.com>
2015-01-13 15:38:37 -0500

    51bp for k-mer trimming; use open-end trimming

--- fermi2.pl
+++ fermi2.pl
@@ -52,24 +52,30 @@ sub mag2fmr {
 }
 
 sub unitig {
-	my %opts = (t=>4, p=>'fmdef', l=>101, k=>-1, e=>0, o=>-1, m=>-1, s=>'100m');
-	getopts('t:p:k:f:r:c:e:l:m:CKEbs:', \%opts);
+	my %opts = (t=>4, p=>'fmdef', l=>101, k=>-1, e=>0, T=>51, o=>-1, m=>-1, s=>'100m');
+	getopts('t:p:k:f:r:c:e:l:m:CKEbs:T:', \%opts);
 	die (qq/
 Usage:   fermi2.pl unitig [options] <in.fq>\n
 Options: -p STR     output prefix [$opts{p}]
-         -s STR     approximate genome size [$opts{s}]
+         -s STR     approximate genome size (for bfc) [$opts{s}]
          -l INT     primary read length [$opts{l}]
-         -e INT     fermi2 ec k-mer length (0 to use bfc) [$opts{e}]
+         -T INT     use INT-mer for post-trimming\/filtering [$opts{T}]
          -k INT     min overlap length during unitig construction [based on -l]
          -o INT     min overlap length during graph cleaning [based on -l]
          -m INT     min overlap length for unambiguous merging [based on -l]
          -t INT     number of threads [$opts{t}]
+
+         -e INT     fermi2 ec k-mer length (0 to use bfc) [$opts{e}]
+         -K         don't drop reads during error correction
          -C         don't cut at low-quality bases for raw reads
          -E         cut at low-quality bases for corrected reads
-         -K         don't drop reads during error correction
 \n/) if (@ARGV == 0);
 
 	my $use_bfc = ($opts{e} == 0);
+
+	if (!$use_bfc) {
+		delete($opts{K}); delete($opts{C}); delete($opts{E});
+	}
 
 	$opts{k} = int($opts{l} * .5)  + 1 if $opts{k} < 0;
 	$opts{m} = int($opts{l} * .75) + 1 if $opts{m} < 0;
@@ -87,7 +93,7 @@ Options: -p STR     output prefix [$opts{p}]
 	my @lines = ();
 	push(@lines, qq/PREFIX=$opts{p}/, '');
 	push(@lines, qq/EXE_FERMI2=$opts{f}/, qq/EXE_ROPEBWT2=$opts{r}/, qq/EXE_BFC=$opts{c}/);
-	push(@lines, qq/K_UNITIG=$opts{k}/, qq/K_EC=$opts{e}/, qq/K_CLEAN=$opts{o}/, qq/K_MERGE=$opts{m}/, qq/GENOME_SIZE=$opts{s}/);
+	push(@lines, qq/K_UNITIG=$opts{k}/, qq/K_EC=$opts{e}/, qq/K_CLEAN=$opts{o}/, qq/K_TRIM=$opts{T}/, qq/K_MERGE=$opts{m}/, qq/GENOME_SIZE=$opts{s}/);
 	push(@lines, qq/N_THREADS=$opts{t}/, "");
 	push(@lines, qq/INPUT=$ARGV[0]/, "") unless ($is_file);
 
@@ -103,7 +109,7 @@ Options: -p STR     output prefix [$opts{p}]
 		push(@lines, "");
 
 		push(@lines, qq/\$(PREFIX).flt.fa.gz:\$(PREFIX).ec.fq.gz/);
-		push(@lines, qq/\t\$(EXE_BFC) -1Qs \$(GENOME_SIZE) -t \$(N_THREADS) \$< 2> \$@.log | gzip -1 > \$@/, "");
+		push(@lines, qq/\t\$(EXE_BFC) -1Qs \$(GENOME_SIZE) -k \$(K_TRIM) -t \$(N_THREADS) \$< 2> \$@.log | gzip -1 > \$@/, "");
 
 		push(@lines, qq/\$(PREFIX).flt.fmd:\$(PREFIX).flt.fa.gz/);
 		push(@lines, qq/\t\$(EXE_ROPEBWT2) -dNCr \$< > \$@ 2> \$@.log/, "");
@@ -131,7 +137,7 @@ Options: -p STR     output prefix [$opts{p}]
 		push(@lines, qq/\t\$(EXE_ROPEBWT2) $opt_rb2 \$< > \$@ 2> \$@.log/, "");
 
 		push(@lines, qq/\$(PREFIX).flt.sub:\$(PREFIX).ec.fmd/);
-		push(@lines, qq/\t\$(EXE_FERMI2) occflt -t \$(N_THREADS) \$< > \$@ 2> \$@.log/, "");
+		push(@lines, qq/\t\$(EXE_FERMI2) occflt -t \$(N_THREADS) -K \$(K_TRIM) \$< > \$@ 2> \$@.log/, "");
 
 		push(@lines, qq/\$(PREFIX).flt.fmd:\$(PREFIX).ec.fmd \$(PREFIX).flt.sub/);
 		push(@lines, qq/\t\$(EXE_FERMI2) sub -ct \$(N_THREADS) \$< \$(PREFIX).flt.sub > \$@ 2> \$@.log/, "");
@@ -141,7 +147,7 @@ Options: -p STR     output prefix [$opts{p}]
 	push(@lines, qq/\t\$(EXE_FERMI2) assemble -l \$(K_UNITIG) -m \$(K_MERGE) -t \$(N_THREADS) \$< 2> \$@.log | gzip -1 > \$@/, "");
 
 	push(@lines, qq/\$(PREFIX).mag.gz:\$(PREFIX).pre.gz/);
-	push(@lines, qq/\t\$(EXE_FERMI2) simplify -CSo \$(K_CLEAN) -m \$(K_MERGE) \$< 2> \$@.log | gzip -1 > \$@/, "");
+	push(@lines, qq/\t\$(EXE_FERMI2) simplify -CSo \$(K_CLEAN) -m \$(K_MERGE) -T \$(K_UNITIG) \$< 2> \$@.log | gzip -1 > \$@/, "");
 
 	print(join("\n", @lines), "\n");
 }
